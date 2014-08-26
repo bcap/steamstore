94f879aac0ad0c5f5274e91c02735fbb4d048602
Bruno Polaco <polaco@gmail.com>
2014-08-26 14:02:44 -0300

    Changing some variables to class variables

--- steamstore/spiders/listing.py
+++ steamstore/spiders/listing.py
@@ -11,19 +11,20 @@ class ListingSpider(scrapy.Spider):
         'http://store.steampowered.com/search/?sort_by=Name&sort_order=ASC',
     )
 
+    url_pattern = re.compile(r'/?\?.*$')
+    type_icon_pattern = re.compile(r'^.*/ico_type_(\w+).gif')
+
+    get_first = lambda self, l: l[0] if l else None
+
+
     def parse(self, response):
-
-        get_first = lambda l: l[0] if l else None
-
-        url_pattern = re.compile(r'/?\?.*$')
-        type_icon_pattern = re.compile(r'^.*/ico_type_(\w+).gif')
 
         for sel in response.xpath('//div[@id="search_result_container"]/div[6]/a'):
             item = ListingItem()
-            item['name'] = get_first(sel.xpath('div[@class="col search_name ellipsis"]/h4/text()').extract())
-            item['type'] = type_icon_pattern.sub(r'\1', get_first(sel.xpath('div[@class="col search_type"]/img/@src').extract()))
-            item['price'] = get_first(sel.xpath('div[@class="col search_price"]/text()').extract())
-            item['url'] = url_pattern.sub('', get_first(sel.xpath('@href').extract()))
+            item['name'] = self.get_first(sel.xpath('div[@class="col search_name ellipsis"]/h4/text()').extract())
+            item['type'] = self.type_icon_pattern.sub(r'\1', self.get_first(sel.xpath('div[@class="col search_type"]/img/@src').extract()))
+            item['price'] = self.get_first(sel.xpath('div[@class="col search_price"]/text()').extract())
+            item['url'] = self.url_pattern.sub('', self.get_first(sel.xpath('@href').extract()))
             yield item
 
             #yield scrapy.Request(item['url'], callback=self.parse_item_page)
